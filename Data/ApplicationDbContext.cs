cc84bd78370171745168e262415baca9c1a586a1
Elvis Chimuse <elvischimuse@gmail.com>
2025-04-30 03:18:38 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Event_Management_System.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Event_Management_System.Models
+{
+    public class AppUser
+    {
+    }
+}
