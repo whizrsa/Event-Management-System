75350d9c7450938d63e153caa4970d04f4c1d522
Elvis Chimuse <elvischimuse@gmail.com>
2025-05-01 15:39:07 +0200

    DbContext Update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Event_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Event_Management_System.Data
 {
@@ -8,5 +9,9 @@ namespace Event_Management_System.Data
         {
 
         }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<RSVP> RSVPs { get; set; }
     }
 }
