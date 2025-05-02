3d26116ad498ce30f489775849adbd6727b61c0d
Elvis Chimuse <elvischimuse@gmail.com>
2025-05-02 02:13:01 +0200

    Register

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,8 @@ namespace Event_Management_System.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            public string FullName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
